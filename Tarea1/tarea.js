63766bb3889e1e11b7b2f28625a2a5a36de7242b
Unknown <ismaelssj120@gmail.com>
2018-02-20 09:28:13 -0500

    Tarea1

---
+++ Tarea1/tarea.js
@@ -0,0 +1,11 @@
+
+"use strict"
+
+while(true){
+let estatura = prompt("Digite su estatura en metros: ")
+let peso = prompt("Digite su peso en Kilogramos: ")
+let resultado = peso/(estatura*estatura);
+ 
+ alert(resultado);
+ 
+}
