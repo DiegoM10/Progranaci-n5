b4fb3c58e3e5015fa920553bfcf2e73b09d6c186
Unknown <ismaelssj120@gmail.com>
2018-02-20 10:23:08 -0500

    clase1

---
+++ clase1/ejemplo1.js
@@ -0,0 +1,38 @@
+"use script"
+
+alert("Hola");
+alert(3  + + 1);
+
+let mensaje;
+mensaje = "Wholo";
+alert(mensaje);
+
+let nombre = "Petra";
+let apellido = "petrov "
+
+
+let num = 123;
+num = 123.456;
+alert(num/0);
+alert (Infinity);
+apellido
+let apodo =`${nombre}"Wila"${apellido}`
+alert(apodo);
+
+let edad = null;
+edad = undefined;
+alert(edad)
+
+alert ("6" / "2");
+let str = "123";
+alert (typeof str);
+let num2 = Number(str);
+alert(typeof num2);
+
+let abc = True;
+abc = String(abc);
+alert(typeof abc);
+//cOMPARACION DE CADENAS
+alert("2">"A");
+alert("Hola">"Holi");
+alert("Zoo">"zo");
